34ced280d0fa874a65976fc7940fbeba25a5f474
zzp <zhangzhengpeng2021@163.com>
2021-07-08 17:39:28 +0800

    品牌直供

--- myshop-browser/src/pages/index/index.vue
+++ myshop-browser/src/pages/index/index.vue
@@ -10,13 +10,33 @@
     </div>
     <!-- 轮播图区域 -->
     <div class="swiper">
-       <swiper class="swiper_container" indicator-dots="true" autoplay="true" interval="3000" circular="true" duration="500">
+       <swiper class="swiper_container" style="height:100%" indicator-dots="true" autoplay="true" interval="3000" circular="true" duration="500">
           <block v-for="(item , index) in banner" :key="index">
-            <swiper-item class="swiper-item">
-               <image class="slide-image" :src="item.image_url"  />
+            <swiper-item class="swiper-item" style="height:100%;width: 100%;">
+               <image class="slide-image" style="width:100%" :src="item.image_url"  />
             </swiper-item>
           </block>
        </swiper>
+    </div>
+    <div class="channel">
+      <div v-for="(item , index) in channel" :key="index" @click="categroyList(item.id)">
+        <img :src="item.icon_url" alt="">
+        <p>{{item.name}}</p>
+      </div>
+    </div>
+    <div class="brand">
+      <div class="head">
+        品牌制造商直供
+      </div>
+      <div class="content">
+        <div v-for="(item , index) in brandList" :key="index" >
+          <div @click="shopList">
+            <p>{{item.name}}</p>
+            <p class="price">{{item.floor_price}}元起</p>
+          </div>
+          <img :src="item.new_pic_url" alt="">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,10 +49,16 @@ import{ get } from '../../utils'
     data(){
       return {
          banner: [],
+         channel:[],
+         brandList:[],
       }
     },
     computed: {
       ...mapState(['cityName'])
+    },
+    mounted(){
+      this.getData()
+      this.getCityNmae()
     },
     methods: {
       ...mapMutations(['update']),
@@ -77,6 +103,14 @@ import{ get } from '../../utils'
       async getData(){
          const data = await get('/index/index')
          console.log(data)
+         this.banner = data.banner 
+         this.channel = data.channel
+         this.brandList = data.brandList
+      },
+      categroyList(id){
+         wx.navigateTo({
+           url:'/pages/categroylist/mian?id='+id
+         })
       }
     }
   }
