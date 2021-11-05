ecd4e671ddb1b668b0200d618466506c91024082
zzp812875759 <zhangzhengpeng2021@163.com>
2021-11-05 15:02:35 +0800

    完成搜索页面渲染以及数据接口

--- myshop-browser/src/pages/search/index.vue
+++ myshop-browser/src/pages/search/index.vue
@@ -3,7 +3,7 @@
       <div class="head">
           <div>
               <img src="http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/search2-2fb94833aa.png"/>
-              <input type="text" confirm-type="search" focus="true" v-model="words" @focus="inputFocus">
+              <input type="text" confirm-type="search" focus="true" v-model="words" @focus="inputFocus" @input="tipsearch" @confirm="searchWords" placeholder="商品搜索">
               <img @click="clearInput" class="del" src="http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/clearIpt-f71b83e3c2.png" />
           </div>
           <div @click="cancel">取消</div>
@@ -39,19 +39,45 @@
 </template>
 
 <script>
+import{ get , post} from '../../utils'
 export default {
     data() {
         return{
             words:'',
+            openid:'',
+            hotData:[],
+            historyData:[],
         }
+    },
+    mounted() {
+        this.openid = wx.getStorage('openid') || '';
+        this.getHotData()
     },
     methods:{
         clearInput(){
             this.words = '';
         },
         cancel(){},
-        clearHistory() {},
-
+        clearHistory(){},
+        inputFocus(){},
+        tipsearch(){},
+        async searchWords(e){
+            let value = e.currentTarget.dataset.value
+            this.words = value || this.words
+            const data = await post('/search/addhistoryaction',{
+                openId:this.words,
+                keyword:value || this.words
+            })
+            // console.log(data)
+            // 获取历史数据
+            this.getHotData()
+        },
+        async getHotData(first) {
+            const data = await get('/search/indexaction?openId=' + this.openid)
+            this.historyData = data.historyData
+            this.hotData = data.hotData
+            console.log(data)
+        }
     }
 }
 </script>
